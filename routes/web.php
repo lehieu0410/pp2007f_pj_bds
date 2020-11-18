60dc3601d8f51880795f300a009f5e4d27c02451
lehieu0410 <leduyhieu0410@gmail.com>
2020-11-18 09:09:09 +0700

    Replace_Imageseeder

--- routes/web.php
+++ routes/web.php
@@ -63,3 +63,7 @@ Route::get('/tu-van-phong-thuy', 'PhongThuyController@index2')->name('tuvanphong
 Route::get('/phong-thuy-nha-o', 'PhongThuyController@index3')->name('phongthuynhao');
 Route::get('/phong-thuy-van-phong', 'PhongThuyController@index4')->name('phongthuyvanphong');
 Route::get('/phong-thuy-theo-tuoi', 'PhongThuyController@index5')->name('phongthuytheotuoi');
+
+// Ckeditor
+Route::resource('/editor', 'CKEditorController');
+Route::post('ckeditor/image_upload', 'CKEditorController@upload')->name('upload');
