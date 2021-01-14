42a315018e3f94dddbd75d201916946439b7e877
huy17git <garunemo3@gmail.com>
2021-01-14 09:50:55 +0700

    fix_ui

--- app/Http/Controllers/TinTucController.php
+++ app/Http/Controllers/TinTucController.php
@@ -47,27 +47,27 @@ class TinTucController extends Controller
         return view('pages.tintucbds.taichinhbds');
     }
 
+
+    public function index() {
+        $articles=$this->articleRepo->index();
+        $recommend = Article::all()->random(8);
+        return view("pages.tintucbds.tintuc", compact('articles','recommend'));
+    }
+
+    public function searchTinTuc(Request $req) {
+        $articles = Article::where('title','LIKE','%'.$req->timtintuc.'%')->orWhere('content','LIKE','%'.$req->timtintuc.'%')->simplePaginate(10);
+
+        return view("pages.tintucbds.tintuc", compact('articles'));
+
+    }
+
     //Singlepost
-
     
-
     public function articles($id) {
         $articles = $this->articleRepo->articles($id);
         $newarticles = Article::orderBy('id', 'desc')->limit(5)->get();
         $sametype = Article::where('type', $articles[0]['type'] )->limit(7)->orderBy('id', 'desc')->get();
         return view("pages.tintucbds.singlepost", compact('articles','newarticles', 'sametype'));
-    }
-
-    public function index() {
-        $articles=$this->articleRepo->index();
-        return view("pages.tintucbds.tintuc", compact('articles'));
-    }
-
-    public function searchTinTuc(Request $req) {
-        $articles = Article::where('title','LIKE','%'.$req->timtintuc.'%')->simplePaginate(10);
-
-        return view("pages.tintucbds.tintuc", compact('articles'));
-
     }
 
     
